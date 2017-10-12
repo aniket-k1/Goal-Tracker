c52b64ba89e00fd838f93fb92fcc0bd1cecf6d1d
Aniket Kulkarni <Aniket.Kulkarni@tdameritrade.com>
2017-10-12 18:46:15 -0400

    Add debug pane and modify scene

--- House-Portal-UnityProject/Assets/UnityARKitPlugin/Plugins/iOS/UnityARKit/UnityARHitTestExample.cs
+++ House-Portal-UnityProject/Assets/UnityARKitPlugin/Plugins/iOS/UnityARKit/UnityARHitTestExample.cs
@@ -40,8 +40,8 @@ namespace UnityEngine.XR.iOS
                         ARHitTestResultType.ARHitTestResultTypeExistingPlaneUsingExtent, 
                         // if you want to use infinite planes use this:
                         //ARHitTestResultType.ARHitTestResultTypeExistingPlane,
-                        ARHitTestResultType.ARHitTestResultTypeHorizontalPlane, 
-                        ARHitTestResultType.ARHitTestResultTypeFeaturePoint
+                        //ARHitTestResultType.ARHitTestResultTypeHorizontalPlane, 
+                        //ARHitTestResultType.ARHitTestResultTypeFeaturePoint
                     }; 
 					
                     foreach (ARHitTestResultType resultType in resultTypes)
