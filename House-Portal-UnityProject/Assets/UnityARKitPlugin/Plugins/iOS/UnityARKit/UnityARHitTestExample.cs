2dbeb280c9fc2d5111f69b4cdf26209db330027a
Aniket Kulkarni <Aniket.Kulkarni@tdameritrade.com>
2017-10-13 16:46:12 -0400

    Clean up dropping portal

--- House-Portal-UnityProject/Assets/UnityARKitPlugin/Plugins/iOS/UnityARKit/UnityARHitTestExample.cs
+++ House-Portal-UnityProject/Assets/UnityARKitPlugin/Plugins/iOS/UnityARKit/UnityARHitTestExample.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using UnityEngine.EventSystems;
 
 namespace UnityEngine.XR.iOS
 {
@@ -27,7 +28,7 @@ namespace UnityEngine.XR.iOS
 			if (Input.touchCount > 0 && m_HitTransform != null)
 			{
 				var touch = Input.GetTouch(0);
-				if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved)
+				if (touch.phase == TouchPhase.Began && !EventSystem.current.IsPointerOverGameObject(0))
 				{
 					var screenPosition = Camera.main.ScreenToViewportPoint(touch.position);
 					ARPoint point = new ARPoint {
@@ -40,8 +41,8 @@ namespace UnityEngine.XR.iOS
                         ARHitTestResultType.ARHitTestResultTypeExistingPlaneUsingExtent, 
                         // if you want to use infinite planes use this:
                         //ARHitTestResultType.ARHitTestResultTypeExistingPlane,
-                        //ARHitTestResultType.ARHitTestResultTypeHorizontalPlane, 
-                        //ARHitTestResultType.ARHitTestResultTypeFeaturePoint
+                        ARHitTestResultType.ARHitTestResultTypeHorizontalPlane, 
+                        ARHitTestResultType.ARHitTestResultTypeFeaturePoint
                     }; 
 					
                     foreach (ARHitTestResultType resultType in resultTypes)
