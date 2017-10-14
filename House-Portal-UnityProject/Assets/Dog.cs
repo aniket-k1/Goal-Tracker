287fa8d7d989a25bf98c48fcd833d5fac9f1f018
Shahzeb Khan <shahzeb.khan@tdameritrade.com>
2017-10-13 20:38:55 -0400

    Adds animation to the dog.

---
+++ House-Portal-UnityProject/Assets/Dog.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dog : MonoBehaviour {
+
+	private Animation dogAnimation;
+
+
+	// Use this for initialization
+	void Start () {
+		dogAnimation = GetComponent<Animation> ();
+
+		if (!dogAnimation.isPlaying) {
+			
+			dogAnimation.Play ();
+		
+		
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!dogAnimation.isPlaying) {
+
+			dogAnimation.Play ();
+		}
+	}
+}
